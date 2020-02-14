4d5289fce4358bb9a145361ec546c09c9dff0885
davidb <davidb.devops@gmail.com>
2020-02-14 15:34:14 -0600

    Update app.js - implement event arugment

--- first-app/app.js
+++ first-app/app.js
@@ -3,11 +3,11 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 // Register a listener
-emitter.on('messageLogged', function(){
-    console.log('Listener called');
+emitter.on('messageLogged', function(e){
+    console.log('Listener called', e);
 });
 
 
 // Raise an event
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', {id: 1, url: 'http://'});
 
